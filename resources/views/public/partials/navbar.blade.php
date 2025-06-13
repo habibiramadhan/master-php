989226dd433588e2e05d28b401a63e8817a3b046
Muhammad Habibi Ramadhan <habibiramadhan.dev@gmail.com>
2025-06-13 18:34:39 +0700

    Menghapus kode debug dari tampilan navbar untuk membersihkan kode dan meningkatkan keterbacaan. Memastikan tampilan tetap berfungsi dengan baik setelah penghapusan.

--- resources/views/public/partials/navbar.blade.php
+++ resources/views/public/partials/navbar.blade.php
@@ -1,13 +1,12 @@
 {{-- resources/views/public/partials/navbar.blade.php --}}
-<!-- DEBUG: Cek data pengaturan -->
-<!-- @php
+@php
     var_dump('=== DEBUG NAVBAR ===');
     var_dump('isset pengaturan:', isset($pengaturan));
     if(isset($pengaturan)) {
         var_dump('pengaturan data:', $pengaturan);
     }
     var_dump('=== END DEBUG ===');
-@endphp -->
+@endphp
 
 <nav class="navbar navbar-expand-lg navbar-dark navbar-construction sticky-top">
     <div class="container">
